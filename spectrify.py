2ea8fbf17800ffa92be9c5566c66d520b6590210
System Administrator <root@Kerosene-IV.local>
2015-01-09 17:16:39 +0000

    Corrected typo

--- spectrify.py
+++ spectrify.py
@@ -1,4 +1,4 @@
-# The aim of this code is to take a 2D numpy array of spectral values (wavelength, counts) and 'reverse engineer' the data to produce a visible spectrum image vaguely like what came out of the telescope spectrograph.  It is intended for mor intuitive visualisation of spectra for public outreach etc.
+# The aim of this code is to take a 2D numpy array of spectral values (wavelength, counts) and 'reverse engineer' the data to produce a visible spectrum image vaguely like what came out of the telescope spectrograph.  It is intended for more intuitive visualisation of spectra for public outreach etc.
 # Actually, any image can be provided - the idea is that the image shows a linear progression of colours within a (specified) wavelength range.
 # The code rescales, truncates and interpolates the spectral data so that it can be pasted over the 'rainbow' image as a mask, where colours with few counts are opaque and colours with the most counts are transparent.  The user can specify the output resolution of the image and a file path for saving.
 
